5d703330f11b89bd581349396970ec7f9e6ec903
ShanJuy <tsigel@vikivostok.ru>
2022-08-08 00:15:17 +0300

    authentification, task edit, task add

--- BoardDir/BoardDir/urls.py
+++ BoardDir/BoardDir/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gendir/', include('gendir.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ]
--- BoardDir/gendir/urls.py
+++ BoardDir/gendir/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from . import  views
+from django.contrib.auth import views as auth_views
 
 app_name = 'gendir'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/logout/', auth_views.LogoutView.as_view()),
+    path('accounts/logout-then-login/', auth_views.logout_then_login),
+    path('new_task', views.new_task, name='new_task'),
+    path('add_task', views.add_task, name='add_task'),
+    path('edit_task/<int:id>', views.edit_task, name='edit_task'),
+    path('edit_task/update_task/<int:id>', views.update_task, name='update_task'),
+
 ]
